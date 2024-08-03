30f3c224d633964a6344e18f70adef0be5645539
Dfacundorod <112908434+Dfacundorod@users.noreply.github.com>
2024-08-03 13:11:26 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let f = 0; f < adj.length; f++) {
+      for (let g = 0; g < noun.length; g++) {
+        for (let h = 0; h < extension.length; h++) {
+          console.log(`${pronoun[i]}${adj[f]}${noun[g]}${extension[h]}`);
+        }
+      }
+    }
+  }
 };
